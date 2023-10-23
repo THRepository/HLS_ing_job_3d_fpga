52feb1cf3eafd538d45f00f768249c0d8eef6386
Tomas Hägg <tomas.hagg.93@gmail.com>
2023-10-23 17:15:11 +0200

    Add folders and initial files

---
+++ hls/build.bat
@@ -0,0 +1,23 @@
+@echo off
+set "SOURCE_FILES="
+set "FPGA_OUTPUT_NAME=ver_1"
+set "FPGA_FLAGS=--quartus-compile"
+set "CLOCK_FLAG=--clock 50MHz"
+set "SIMULATION_FLAG="
+
+if not "%1"=="" (
+  set "TARGET=%1"
+) else (
+  set "TARGET=emulate"
+)
+
+if "%TARGET%" == "emulate" (
+  set "CMD=i++ -march=x86-64 -o %TARGET%.exe" 
+) else if "%TARGET%" == "fpga" (
+  set "CMD=i++ -march=MAX10 %FPGA_FLAGS% %CLOCK_FLAG% %SIMULATION_FLAG% -o %FPGA_OUTPUT_NAME%.exe"
+)
+set "CMD=%CMD:""="%"
+
+echo %CMD% %SOURCE_FILES%
+echo:
+%CMD% %SOURCE_FILES%
