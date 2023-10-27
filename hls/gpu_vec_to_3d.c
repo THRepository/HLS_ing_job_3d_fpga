23edb6cb48194422485960711e253b3a28a827f5
Tomas Hägg <tomas.hagg.93@gmail.com>
2023-10-27 13:08:26 +0200

    normals for every triangle calculated

--- hls/gpu_vec_to_3d.c
+++ hls/gpu_vec_to_3d.c
@@ -33,10 +33,17 @@ void gpu_3d(ihc::mm_master<float, ihc::aspace<1>, ihc::awidth<32>, ihc::dwidth<3
     hls_register
     float vec[3];
 
+    vec_3d tri_buffer[3];
+    vec_3d b, c;
+    vec_3d normals;
+    float n;
+
     hls_register
     int sample_size = end_addr - start_addr;
 
     float buff;
+
+    int tri_buffer_counter = 0;;
 
     for(int i = 0; i < sample_size;)
     {
@@ -60,9 +67,52 @@ void gpu_3d(ihc::mm_master<float, ihc::aspace<1>, ihc::awidth<32>, ihc::dwidth<3
         vec[1] = vec[1] * PSI_matrix[0][1] + vec[1] * PSI_matrix[1][1] + vec[1] * PSI_matrix[2][1];
         vec[2] = vec[2] * PSI_matrix[0][2] + vec[2] * PSI_matrix[1][2] + vec[2] * PSI_matrix[2][2];
 
-        out_memory[i    ] = vec[0];
-        out_memory[i + 1] = vec[1];
-        out_memory[i + 2] = vec[2];
+        // Collect vectors for one triangle.
+        tri_buffer[tri_buffer_counter].x = vec[0];
+        tri_buffer[tri_buffer_counter].x = vec[1];
+        tri_buffer[tri_buffer_counter].x = vec[2];
+
+        
+        if(tri_buffer_counter < 2){
+            tri_buffer_counter += 1;
+        }else{
+            // Calculate cross product for triangle.
+            b.x = tri_buffer[1].x - tri_buffer[0].x;
+            b.y = tri_buffer[1].y - tri_buffer[0].y;
+            b.z = tri_buffer[1].z - tri_buffer[0].z;
+
+            c.x = tri_buffer[2].x - tri_buffer[0].x;
+            c.y = tri_buffer[2].y - tri_buffer[0].y;
+            c.z = tri_buffer[2].z - tri_buffer[0].z;
+
+            normals.x = b.y * c.z - b.z * c.y;
+            normals.y = b.z * c.x - b.x * c.z;
+            normals.z = b.x * c.y - b.y * c.x;
+
+            n = sqrt(normals.x * normals.x + 
+                     normals.y * normals.y + 
+                     normals.z * normals.z);
+            
+            normals.x /= n;
+            normals.y /= n;
+            normals.z /= n;
+
+            printf("Normals %.6f | %.6f | %.6f \n", normals.x, normals.y, normals.z);
+
+            out_memory[i    ] = tri_buffer[0].x;
+            out_memory[i + 1] = tri_buffer[0].y;
+            out_memory[i + 2] = tri_buffer[0].z;
+            out_memory[i + 3] = tri_buffer[1].x;
+            out_memory[i + 4] = tri_buffer[1].y;
+            out_memory[i + 5] = tri_buffer[1].z;
+            out_memory[i + 6] = tri_buffer[2].x;
+            out_memory[i + 7] = tri_buffer[2].y;
+            out_memory[i + 8] = tri_buffer[2].z;
+        }
+
+        //out_memory[i    ] = vec[0];
+        //out_memory[i + 1] = vec[1];
+        //out_memory[i + 2] = vec[2];
         i += 3;
     }
 }
