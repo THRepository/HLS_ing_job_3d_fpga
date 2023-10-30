d1a45fe100301ce3edf4a97efd0d30f01fd47f43
Tomas Hägg <tomas.hagg.93@gmail.com>
2023-10-30 16:16:36 +0100

    Happy with algoritm so far.

--- hls/gpu_polygon.c
+++ hls/gpu_polygon.c
@@ -5,15 +5,15 @@
 #define psi 2
 
 component
-void gpu_polygon(uint8 addr,
+void gpu_polygon(hls_stable_argument uint8 addr,
                  input_memory_type  &in_memory,
                  var_memory_type    &var_memory,
                  output_memory_type &out_memory){
-    fixed_16_9_t vectors[10];
-    fixed_16_9_t vectors_copy[9];
-    fixed_16_9_t vars[9];
-    fixed_16_9_t sin[3];
-    fixed_16_9_t cos[3];
+    hls_register fixed_16_9_t vectors[10];
+    hls_register fixed_16_9_t vectors_copy[9];
+    hls_register fixed_16_9_t vars[9];
+    hls_register fixed_16_9_t sin[3];
+    hls_register fixed_16_9_t cos[3];
     fixed_16_9_t rot_phi[3][3];
     fixed_16_9_t rot_theta[3][3];
     fixed_16_9_t rot_psi[3][3];
@@ -24,20 +24,50 @@ void gpu_polygon(uint8 addr,
         vectors[i] = in_memory[addr + i];
         vars[i]    = var_memory[i];
     }
-
-    for(uint2 i = 0; i < 3; i++)
+    #pragma unroll 1
+    for(uint3 i = 0; i < 3; i++)
     {
         sin[i] = sinpi_fixed(var_memory[i + 3]);
         cos[i] = cospi_fixed(var_memory[i + 3]);
     }
 
     #pragma unroll 1
-    for(uint2 i = 0; i < 3; i++)
+    for(uint3 i = 0; i < 3; i++)
     {
         vectors[i*3]     = vectors[i*3]     + var_memory[0] - var_memory[6];
         vectors[i*3 + 1] = vectors[i*3 + 1] + var_memory[1] - var_memory[7];
         vectors[i*3 + 2] = vectors[i*3 + 2] + var_memory[2] - var_memory[8];
     }
+
+    // #pragma unroll 1
+    // for(uint3 i = 0; i < 3;i++ )
+    // {
+    //     vectors_copy[i*3 + 1] = vectors[i*3 + 1];
+    //     vectors_copy[i*3 + 2] = vectors[i*3 + 2];
+
+    //     vectors[i*3 + 1] = vectors_copy[i*3 + 1] * cos[phi] - vectors_copy[i*3 + 2] * sin[phi];
+    //     vectors[i*3 + 2] = vectors_copy[i*3 + 2] * cos[phi] + vectors_copy[i*3 + 1] * sin[phi];
+    // }
+
+    // #pragma unroll 1
+    // for(uint3 i = 0; i < 3;i++ )
+    // {
+    //     vectors_copy[i*3    ] = vectors[i*3    ];
+    //     vectors_copy[i*3 + 2] = vectors[i*3 + 2];
+
+    //     vectors[i*3    ] = vectors_copy[i*3    ] * cos[theta] - vectors_copy[i*3 + 2] * sin[theta];
+    //     vectors[i*3 + 2] = vectors_copy[i*3 + 2] * cos[theta] + vectors_copy[i*3    ] * sin[theta];
+    // }
+
+    // #pragma unroll 1
+    // for(uint3 i = 0; i < 3;i++ )
+    // {
+    //     vectors_copy[i*3    ] = vectors[i*3    ];
+    //     vectors_copy[i*3 + 1] = vectors[i*3 + 1];
+
+    //     vectors[i*3    ] = vectors_copy[i*3    ] * cos[psi] - vectors_copy[i*3 + 1] * sin[psi];
+    //     vectors[i*3 + 1] = vectors_copy[i*3 + 1] * cos[psi] + vectors_copy[i*3    ] * sin[psi];
+    // }
 
     rot_phi[0][0] = 1.0;
     rot_phi[0][1] = 0.0;
@@ -80,14 +110,16 @@ void gpu_polygon(uint8 addr,
         // vectors[3*i + 2] *= cos[phi]*sin[theta]*cos[psi] + sin[phi]*sin[psi] 
         //                   + cos[phi]*sin[theta]*sin[psi] - sin[phi]*cos[psi] 
         //                   + cos[phi]*cos[theta];
+
+
     #pragma max_concurrency 1
-    for(uint2 i = 0; i < 3; i++)
+    for(uint3 i = 0; i < 3; i++)
     {   
         vectors_copy[i*3    ] = 0.0;
         vectors_copy[i*3 + 1] = 0.0;
         vectors_copy[i*3 + 2] = 0.0;
         #pragma max_concurrency 1
-        for(uint2 row = 0; row < 3; row++)
+        for(uint3 row = 0; row < 3; row++)
         {
             vectors_copy[i*3    ] += vectors[i*3    ] * rot_phi[0][row];
             vectors_copy[i*3 + 1] += vectors[i*3 + 1] * rot_phi[1][row];
@@ -98,13 +130,13 @@ void gpu_polygon(uint8 addr,
         vectors[i*3 + 2] = vectors_copy[i*3 + 2];
     }
     #pragma max_concurrency 1
-    for(uint2 i = 0; i < 3; i++)
+    for(uint3 i = 0; i < 3; i++)
     {   
         vectors_copy[i*3    ] = 0.0;
         vectors_copy[i*3 + 1] = 0.0;
         vectors_copy[i*3 + 2] = 0.0;
         #pragma max_concurrency 1
-        for(uint2 row = 0; row < 3; row++)
+        for(uint3 row = 0; row < 3; row++)
         {
             vectors_copy[i*3    ] += vectors[i*3    ] * rot_theta[0][row];
             vectors_copy[i*3 + 1] += vectors[i*3 + 1] * rot_theta[1][row];
@@ -115,13 +147,13 @@ void gpu_polygon(uint8 addr,
         vectors[i*3 + 2] = vectors_copy[i*3 + 2];
     }
     #pragma max_concurrency 1
-    for(uint2 i = 0; i < 3; i++)
+    for(uint3 i = 0; i < 3; i++)
     {   
         vectors_copy[i*3    ] = 0.0;
         vectors_copy[i*3 + 1] = 0.0;
         vectors_copy[i*3 + 2] = 0.0;
         #pragma max_concurrency 1
-        for(uint2 row = 0; row < 3; row++)
+        for(uint3 row = 0; row < 3; row++)
         {
             vectors_copy[i*3    ] += vectors[i*3    ] * rot_psi[0][row];
             vectors_copy[i*3 + 1] += vectors[i*3 + 1] * rot_psi[1][row];
@@ -133,7 +165,7 @@ void gpu_polygon(uint8 addr,
     }
 
     #pragma unroll 1
-    for(uint2 i = 0; i < 3; i++)
+    for(uint3 i = 0; i < 3; i++)
     {
         vectors[3*i]     += var_memory[6];
         vectors[3*i + 1] += var_memory[7];
@@ -145,7 +177,7 @@ void gpu_polygon(uint8 addr,
     fixed_16_9_t normal[3];
 
     #pragma unroll 1
-    for(uint2 i = 0; i < 3; i++) // tree edges.
+    for(uint3 i = 0; i < 3; i++) // tree edges.
     {
         b[i] = vectors[i + 3] - vectors[i];
         c[i] = vectors[i + 6] - vectors[i];
@@ -184,3 +216,5 @@ void gpu_polygon(uint8 addr,
         out_memory[i] = vectors[i].to_ac_int();
     }
 }
+
+
